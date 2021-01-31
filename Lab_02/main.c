fe4ff3d9d803fe74f8faa9e9312d2a3a6b6c1c6c
Jacob Mealey <jacob.mealey@maine.edu>
2021-01-31 11:45:09 -0800

    almost working keypad_scan function. need a break

--- Lab_02/main.c
+++ Lab_02/main.c
@@ -21,27 +21,68 @@ uint8_t Data_Send[6];
 #define gpioc_mode 0x3F00300;
 
 void I2C_GPIO_init(void);
-	
+unsigned char keypad_scan();
+
 int main(void){
 	char message[64] = "ABCDEFGHIJK";
 	
 	System_Clock_Init(); // Switch System Clock = 80 MHz
 	I2C_GPIO_init();
-	I2C_Initialization(I2C1);
+	//I2C_Initialization(I2C1);
 
 	//ssd1306_TestAll();
-	ssd1306_Init();
-	ssd1306_Fill(Black);
-	ssd1306_SetCursor(2,0);
-	ssd1306_WriteString(message, Font_16x26, White);		
+	//ssd1306_Init();
+	//ssd1306_Fill(Black);
+	//ssd1306_SetCursor(2,0);
+	//ssd1306_WriteString(message, Font_16x26, White);		
 		
-	while(1);	 // Deadloop
+	while(1) {
+		keypad_scan();
+	}	 // Deadloop
 }
 
 // Configure I2C1_SCL (PB8) Pin as : Alternate function, High Speed, Open drain, Pull up 
 // Configure I2C1_SDA (PB9) Pin as : Alternate function, High Speed, Open drain, Pull up
 // PB 8 <--> AF4 (I2C1_SCL)
 // PB 9 <--> AF4 (I2C1_SDA)
+
+unsigned char keypad_scan(void){
+	unsigned char ColumnPressed, RowPressed;
+	unsigned char key = 0xFF;
+	unsigned int row_masks[] = {
+		GPIO_ODR_ODR_0, 
+		GPIO_ODR_ODR_1, 
+		GPIO_ODR_ODR_2, 
+		GPIO_ODR_ODR_3
+	};
+	
+	unsigned int col_masks[] = {
+		GPIO_IDR_IDR_4,
+		GPIO_IDR_IDR_10,
+		GPIO_IDR_IDR_11,
+		GPIO_IDR_IDR_12
+	};
+	//scan columns
+	int i;
+	int j;
+	for(i = 0; i < 4; i++){
+		if((GPIOC->IDR & col_masks[i]) == col_masks[i]){
+			ColumnPressed = i;
+		}
+	}
+	
+	for(j = 0; j < 4; j++){
+		GPIOC->ODR = row_masks[j];
+		for(i = 0; i < 100; i++);
+		if((GPIOC->IDR & col_masks[ColumnPressed]) == col_masks[ColumnPressed]){
+			RowPressed = j;
+		}
+	}
+	
+	RowPressed =0;
+	ColumnPressed =0;
+	return key;
+}
 
 void I2C_GPIO_init(void){
 	
