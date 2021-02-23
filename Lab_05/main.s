54ac7bf3158e71896dd68d5285d71a419c784130
Jacob Mealey <jacob.mealey@maine.edu>
2021-02-23 16:59:25 -0500

    Pin initial

--- Lab_05/main.s
+++ Lab_05/main.s
@@ -19,6 +19,10 @@
 
 ; Green LED <--> PA.5
 LED_PIN	EQU	5
+MOTOR_A_PIN EQU 5
+MOTOR_NA_PIN EQU 6
+MOTOR_B_PIN EQU 8
+MOTOR_NB_PIN EQU 9
 	
 	AREA    main, CODE, READONLY
 	EXPORT	__main				; make __main visible to linker
@@ -34,15 +38,21 @@ __main	PROC
 	
 	; MODE: 00: Input mode, 01: General purpose output mode
     ;       10: Alternate function mode, 11: Analog mode (reset state)
-	LDR r0, =GPIOA_BASE
+	LDR r0, =GPIOC_BASE
 	LDR r1, [r0, #GPIO_MODER]
-	EOR r1, r1, #(3<<(2*LED_PIN))
-	ORR r1, r1, #(1<<(2*LED_PIN))
+	EOR r1, r1, #(3<<(2*MOTOR_A_PIN))
+	ORR r1, r1, #(1<<(2*MOTOR_A_PIN))
+	EOR r1, r1, #(3<<(2*MOTOR_NA_PIN))
+	ORR r1, r1, #(1<<(2*MOTOR_NA_PIN))
+	EOR r1, r1, #(3<<(2*MOTOR_B_PIN))
+	ORR r1, r1, #(1<<(2*MOTOR_B_PIN))
+	EOR r1, r1, #(3<<(2*MOTOR_NB_PIN))
+	ORR r1, r1, #(1<<(2*MOTOR_NB_PIN))
 	STR r1, [r0, #GPIO_MODER]
+	
+	MOV r4, #0x56A9
 
-	LDR r1, [r0, #GPIO_ODR]
-	ORR r1, r1, #(1<<LED_PIN)
-	STR r1, [r0, #GPIO_ODR]
+	
   
 stop 	B 		stop     		; dead loop & program hangs here
 
