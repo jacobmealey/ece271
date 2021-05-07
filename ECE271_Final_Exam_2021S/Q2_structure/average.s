cd265466ea7532f0b06b2cfe280e19ebfd382f37
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-05-06 18:43:29 -0700

    Problem 2 finished - needs more comments

--- ECE271_Final_Exam_2021S/Q2_structure/average.s
+++ ECE271_Final_Exam_2021S/Q2_structure/average.s
@@ -1,7 +1,24 @@
 	AREA    fun, CODE, READONLY, ALIGN=3
-		
+	
+STUDENT_C1 EQU 0
+STUDENT_SCORE EQU 4
+STUDENT_C2 EQU 8	
+STUDENT_SIZE EQU 12
 find_avg_score PROC
+	push {lr, r4, r5}
+	MOV r3, r0
+	MOV r4, #0 ; incrementer 
+	MOV r5, #0 ; sum of all scores
 	EXPORT find_avg_score
-		
+WHILE	; Loop through the array to sum scores
+		LDR r2, [r3, #STUDENT_SCORE] ; load Score into r2
+		ADD r3, r3, #STUDENT_SIZE	; increment
+		ADD r5, r5, r2 ;add whats in r3 to our r5
+		ADD r4, #1	; Increment r4
+		CMP r4, r1	; compare r4 with the max
+	BLT WHILE
+	UDIV r0, r5, r1 ; divide sum by max length 
+	pop {pc, r4, r5}
+	BX lr
 	ENDP
 	END
--- ECE271_Final_Exam_2021S/Q2_structure/main.c
+++ ECE271_Final_Exam_2021S/Q2_structure/main.c
@@ -1,11 +1,15 @@
 #include "stm32l476xx.h"
 
+// This struct is of sizeof(students[0])
+// 6 bytes packed packed and 12 bytes unpacked? 
+
 struct Student_T {
  char c1;
  int score;
  char c2;
 } students[10];
 
+int l;
 extern int find_avg_score(struct Student_T *p, int num_of_students);
 
 void init_structure(){
@@ -19,11 +23,10 @@ void init_structure(){
 int main(void){
 
 	volatile int avg;
-
 	init_structure();
 	
 	avg = find_avg_score(&students[0], 10);
-
+	
 	while(1);
 }
 
