60e53ef25ab33e1416fb48c79f7b912834612d72
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-05-06 19:20:11 -0700

    Question 4 done - time for 3 :(

--- ECE271_Final_Exam_2021S/Q4_compare64/compare64.s
+++ ECE271_Final_Exam_2021S/Q4_compare64/compare64.s
@@ -2,6 +2,19 @@
 		
 compare64 PROC
 	EXPORT compare64
-		
+	PUSH {lr}
+	CMP r1, r3	; compare upper 32
+	BEQ LOWER
+	MOVGT r0, #1
+	MOVLT r0, #-1
+	B SKIP	;We don't need to check lower
+LOWER
+		CMP r0, r2 	; compare lower 32
+		MOVEQ r0, #0
+		MOVGT r0, #1
+		MOVLT r0, #-1
+SKIP
+	POP {pc}
+	BX lr
 	ENDP
 	END
