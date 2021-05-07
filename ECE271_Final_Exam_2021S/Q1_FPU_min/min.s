7ec338abf2c61cb8a062d363455b4dda7273017a
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-05-06 17:02:10 -0700

    Wrote the first question needs testing

--- ECE271_Final_Exam_2021S/Q1_FPU_min/min.s
+++ ECE271_Final_Exam_2021S/Q1_FPU_min/min.s
@@ -1,7 +1,18 @@
 	AREA    fun, CODE, READONLY, ALIGN=3
 		
 min PROC
+	PUSH {LR}
 	EXPORT min
-		
+		VCMPE.F32 s0, s1		; Compare s0 with s1
+		VMRS APSR_nzcv, FPSCR
+		VMOVGT.F32 s0, s1		; Mov it if its  greater
+		VCMPE.F32 s0, s2		; Compare previous one with the second val
+		VMRS APSR_nzcv, FPSCR
+		VMOVGT.F32 s0, s2		; Mov it if its greater
+		VCMPE.F32 s0, s3		; compare the previous min with current
+		VMRS APSR_nzcv, FPSCR
+		VMOVGT.F32 s0, s3		; MOV if its true
+	POP {LR}
+	BX LR
 	ENDP
 	END
