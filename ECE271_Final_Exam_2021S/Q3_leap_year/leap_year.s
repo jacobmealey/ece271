5c9b3244aa1d31d9abbc55a5d61a1f641d7d9531
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-05-06 20:12:33 -0700

    QUESTION 3 DONE LETS GOOOOOOOOOOOOOOOOOOOOOOOOO

--- ECE271_Final_Exam_2021S/Q3_leap_year/leap_year.s
+++ ECE271_Final_Exam_2021S/Q3_leap_year/leap_year.s
@@ -2,6 +2,26 @@
 		
 leap_year PROC
 	EXPORT leap_year
-		MOV r1, r0 MOD 400 
+		MOV r2, #400	; Mod year by 400
+		UDIV r3, r0, r2
+		MLS r3, r2, r3, r0
+		CBZ r3, LEAP
+		
+		MOV r2, #4		; Mod 4 
+		UDIV r3, r0, r2
+		MLS r3, r2, r3, r0
+		CBNZ r3, NOLEAP
+		MOV r2, #100	; Mod 100
+		UDIV r3, r0, r2
+		MLS r3, r2, r3, r0
+		CBZ r3, NOLEAP
+		
+LEAP
+	MOV r0, #1
+	B SKIP
+NOLEAP
+	MOV r0, #0
+SKIP
+	BX LR
 	ENDP
 	END
